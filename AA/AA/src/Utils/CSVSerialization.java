9836f31be123c7011f7cc5a0f149975617f1e490
AMAPuiu <anapuiu99@gmail.com>
2020-05-06 10:18:50 +0300

    deleted double git push

--- AA/AA/src/Utils/CSVSerialization.java
+++
@@ -1,7 +0,0 @@
-package Utils;
-
-public interface CSVSerialization {
-    String [] getColumns(); // returneaza numele coloanelor specifice fiecarei clase pt a putea fi scrise in csv
-    String [] transformToString(String user); // transforma atributele unui obiect in array de stringuri
-    String transformFromString(String [] data); // updateaza un obiect
-}
